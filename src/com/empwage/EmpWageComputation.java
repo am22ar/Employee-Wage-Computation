5cc5177258490ab78afa5045df462176b98fc3c4
am22ar <amarpatil12222@gmail.com>
2022-09-19 12:51:10 +0530

    Added: Employee PartTime Wage Logic

--- src/com/empwage/EmpWageComputation.java
+++ src/com/empwage/EmpWageComputation.java
@@ -8,19 +8,26 @@ public class EmpWageComputation
 	{
 		int empWagePerHour = 20;
 		int fullDayHour = 8;
+		int halfDayHour = 4;
 		int salary = 0;
 		System.out.println("Welcome to Employee Wage Computation Program");
 		Random random = new Random();
-		int empCheck = random.nextInt(2);
+		int empCheck = random.nextInt(3);
 		if(empCheck == 1)
 		{
 			System.out.println("Employee is Present.");
 			salary = empWagePerHour * fullDayHour ;
 		}
+		else if(empCheck == 2)
+		{
+			System.out.println("Present for Half Day");
+			salary = empWagePerHour * halfDayHour ;
+		}
 		else
 		{
 			System.out.println("Employee is Absent.");
 		}
+		System.out.println("random: "+empCheck);
 		System.out.println("Salary is: "+salary);
 	}
 
